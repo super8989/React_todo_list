3510621f161ffd092b0a257b16d7dcbe3fc0bd3b
Sam <super8989@gmail.com>
2020-01-05 20:25:02 -0500

    set up app component structure

--- src/App.js
+++ src/App.js
@@ -38,9 +38,24 @@ class App extends Component {
 
 	render() {
 		return (
-			<div className='App'>
-				<TodoInput />
-				<TodoList />
+			<div className='container'>
+				<div className='row'>
+					<div className='col-10 mx-auto col-md-8 mt-5'>
+						<h3 className='text-capitalize text-center'>Todo Input</h3>
+						<TodoInput
+							item={this.state.item}
+							handleChange={this.handleChange}
+							handleSubmit={this.handleSubmit}
+							editItem={this.state.editItem}
+						/>
+						<TodoList
+							items={this.state.items}
+							clearList={this.clearList}
+							handleDelete={this.handleDelete}
+							handleEdit={this.handleEdit}
+						/>
+					</div>
+				</div>
 			</div>
 		);
 	}
